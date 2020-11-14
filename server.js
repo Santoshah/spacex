b5c099296401ed35116e0b722da269442425465a
Santosh Shah <sks7yu@gmail.com>
2020-11-14 08:20:49 +0530

    fix angular.json

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/space-x'));
+app.use(express.static('/dist/space-x'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/space-x/index.html'));});
+res.sendFile(path.join('/dist/space-x/index.html'));});
 app.listen(process.env.PORT || 8080);
